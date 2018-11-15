e15979d4ef5a85a5b8481a5074efad45d3271a08
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-15 12:18:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ app.appendChild(image);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://www.themuse.com/api/public/jobs', true);
+request.open('GET', 'https://www.themuse.com/api/public/jobs=1', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
