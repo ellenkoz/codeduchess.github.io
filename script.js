257b2223d2172857fdd4b149517337d0e7184841
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-15 13:03:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(image);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
