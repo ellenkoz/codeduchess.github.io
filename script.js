29e65e3a83aca58967eb05a18caa415650d5d63b
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-20 11:07:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = job.title;
+      h1.textContent = jobs.title;
 
       const p = document.createElement('p');
       // original code:  job.description = job.description.substring(0, 300);//
-      job.description = JSON.stringify(job.description).substring(0, 300);
-      p.textContent = `${job.description}...`;
+      jobs.description = JSON.stringify(jobs.description).substring(0, 300);
+      p.textContent = `${jobs.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
