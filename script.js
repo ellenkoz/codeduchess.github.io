8c13af71a6563b6608dd14a53633aa9951ccfc7b
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-20 17:55:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//Create a request varibale and assign a new XMLHttpRequest object
+var request = new XMLHttpRequest();
 
+//Open a new connection, using GET request on the URL endpoint
+request.open('GET', "https://www.themuse.com/api/public/jobs?page=1', true);
+             
+request.onload = function () {
+ // begin accessing JSON data here 
+ }
+}
+
+// send request
+request.send();
