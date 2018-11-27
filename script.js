3ccbf3d92af74143b40556a7caad2bc5cba5e460
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-27 10:32:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ var data = JSON.stringify(this.response);
 
 //create a data variable that contains all the JSON as an array using forEach()
 if (typeof data === 'undefined') {
+  else
+    alert("Undefined");
+  
   data.forEach(job => {
   // log each job title
      console.log(job,title)}
-   )}
-  else
-    alert("undefined");
-
+   )};
+  
