04e7171ec57f5a3f99a3acbd622c9ee89c5702f7
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-28 10:30:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 const app = document.getElementById('root');
 
-//const logo = document.createElement('img');
-//logo.src = 'logo.png';
+const logo = document.createElement('img');
+logo.src = 'logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-//if (typeof logo !== undefined) {
-//app.appendChild(logo);
-//app.appendChild(container);
+if (typeof logo !== undefined) {
+app.appendChild(logo);
+app.appendChild(container);
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
