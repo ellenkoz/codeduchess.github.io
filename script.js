fc0a58dc1156ef6933d93e57419684d4464a0644
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-28 07:15:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-
 const app = document.getElementById('root');
 
-//const logo = document.createElement('img');
-//logo.src = 'logo.png';
+const logo = document.createElement('img');
+logo.src = 'logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-//app.appendChild('img');
-//app.appendChild(container);
+app.appendChild(logo);
+app.appendChild(container);
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function (
-// Begin accessing JSON data here
-var data = JSON.parse(this.response);
-
-data.forEach(movie => {
-// Log each movie's title
-console.log(movie.title);
-});) 
+request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
