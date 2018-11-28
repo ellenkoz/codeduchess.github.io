2c220a882356ef399e4857c7aa4ba2609b6785f9
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-28 07:29:16 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ logo.src = 'logo.png';
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
+document.body.appendChild(paragraph);
 app.appendChild(logo);
 app.appendChild(container);
 
