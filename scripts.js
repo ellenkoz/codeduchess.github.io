5131a7971a1fc5ba671fbc917d4833b227f77de6
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-28 21:11:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,16 +17,16 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(jobs => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = job.title;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      movie.description = job.description.substring(0, 300);
+      p.textContent = `${job.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
@@ -34,7 +34,7 @@ request.onload = function () {
     });
   } else {
     const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
+    errorMessage.textContent = `Hug It, it's not working!`;
     app.appendChild(errorMessage);
   }
 }
