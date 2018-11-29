2741b8b131a177c63680e2b07b77cad4811656f4
Dana L. Brown <dana.l.brown@gmail.com>
2018-11-28 21:24:27 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,16 +17,16 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(jobs => {
+    data.forEach(movie => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = job.title;
+      h1.textContent = movie.title;
 
       const p = document.createElement('p');
-      job.description = job.description.substring(0, 300);
-      p.textContent = `${job.description}...`;
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
