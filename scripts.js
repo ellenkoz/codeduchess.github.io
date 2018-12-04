581f1540994f553bae3b4c6a80c36a683b7c9f47
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-03 21:07:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,29 @@
    
-const app = document.getElementById('root');
+const app = document.getElementById("root");
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+const logo = document.createElement("img");
+logo.src = "logo.png";
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+const container = document.createElement("div');
+container.setAttribute("class", "container");
 
 app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://jobs.search.gov/jobs/search.json?query=analyst+jobs', true);
+request.open("GET", "https://jobs.search.gov/jobs/search.json?query=analyst+jobs", true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach(job => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-
-      const h1 = document.createElement('h1');
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+      const h1 = document.createElement("h1");
       h1.textContent = job.title;
 
-      const p = document.createElement('p');
+      const p = document.createElement("p");
       job.description = JSON.parse(job.description).substring(0, 300); 
       p.textContent = `${job.description}...`;
 
@@ -33,7 +32,7 @@ request.onload = function () {
       card.appendChild(p);
     });
   } else {
-    const errorMessage = document.createElement('marquee');
+    const errorMessage = document.createElement("marquee");
     errorMessage.textContent = `Hug It, it's not working!`;
     app.appendChild(errorMessage);
   }
