7c1f9b39bbda9e15e2d2f0271719c92561a6377c
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-05 12:50:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ request.onload = function () {
 
   // Begin accessing JSON data here
    'use strict';
- var data = JSON(this.response);
+ var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.weather.forEach(job => {
       const card = document.createElement("div");
