909984b11aad3b5ea70bed69d3cfe3c2bfb2616b
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-05 21:50:46 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ var data = JSON.parse(this.response);
       const card = document.createElement("div");
       card.setAttribute("class", "card");
       const h1 = document.createElement("h1");
-      h1.textContent = job.title;
+      h1.textContent = weather.title;
 
       const p = document.createElement("p");
       weather.description =JSON.parse(weather.description).substring(0,300); 
