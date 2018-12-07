b2823b396970ce79d497c4d94ac9c873872e171e
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-07 14:52:21 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ app.appendChild(weather);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open("GET", "https://api.openweathermap.org/data/2.5/forecast?q={Raleigh},{us}&APPID=14d276f4fe655e659ec92149c7cebbec&units=metric", true);
+request.open("GET", "https://api.openweathermap.org/data/2.5/forecast?q={Raleigh},{us}&APPID=14d276f4fe655e659ec92149c7cebbec", true);
 
 request.onload = function () {    
   
