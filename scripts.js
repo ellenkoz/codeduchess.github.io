6c67ad50fd4d40aa79afd75bc9f0aedd83d780d1
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-06 21:16:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,9 +12,9 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open("GET", "https://api.openweathermap.org/data/2.5/group?id=27612,27702,27516,27519&units=metric&
-APPID=14d276f4fe655e659ec92149," true)
-               
+request.open("GET", "https://api.openweathermap.org/data/2.5/group?id=27612,27702,27516,27519&units=metric&APPID=14d276f4fe655e659ec92149", true);
+
+request.onload = function () {               
   // Begin accessing JSON data here
    'use strict';
 var data = JSON.parse(this.response);
