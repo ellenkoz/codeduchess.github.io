df046a51f2e710f4602762f0f0d642c5189128b3
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-10 13:23:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,18 @@
 	'use strict';
 	const app = document.getElementById("root");
 	
-
 	const weather = document.createElement("img");
 	weather.src = "weather.jpg";
 	
-
 	const container = document.createElement("div");
 	container.setAttribute("class", "container");
 	
-
 	app.appendChild(weather);
 	app.appendChild(container);
 	
-
 	var request = new XMLHttpRequest();
 	request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=14d276f4fe655e659ec92149c7cebbec", true);
 	
-
 	request.onload = function () {    
 	  
 	  // Begin accessing JSON data here
@@ -30,12 +25,10 @@
 	      const h1 = document.createElement("h1");
 	      h1.textContent = weather.title;
 	
-
 	      const p = document.createElement("p");
 	      weather.description = weather.description.substring(0, 300);
 	      p.textContent = `${weather.description}...`;
 	
-
 	      container.appendChild(card);
 	      card.appendChild(h1);
 	      card.appendChild(p);
@@ -47,8 +40,4 @@
 	  }
 	}
 	
-
 	request.send();
-
-	
-
