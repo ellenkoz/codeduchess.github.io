ec7f8555234a03cabaf17097b173a6db74081023
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-10 13:04:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,8 +30,8 @@
 	      p.textContent = `${weather.description}...`;
 		    
 	      const t = document.createElement("p");
-	      weather.main:temp = weather.main:temp;
-	      t.textContent = `${weather.main:temp}...`;
+	      weather.main.temp = weather.main.temp;
+	      t.textContent = `${weather.main.temp}...`;
 	
 	      container.appendChild(card);
 	      card.appendChild(h1);
