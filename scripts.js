498295cf19515febcf41ae34553bf2432acfe15f
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-10 10:11:27 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,11 +32,10 @@ var data = JSON.parse(this.response);
       weather.description = weather.description.substring(0, 300);
       p.textContent = `${weather.description}...`;
       
-     // const t = document.createElement("t");
-    //  weather.temp = weather.temp.substring(0,300);
-    //  t.textContent = `${weather.temp}...`;
+      const t = document.createElement("t");
+      weather.temp = weather.temp.substring(0,300);
+      t.textContent = `${weather.temp}...`;
    
-
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
