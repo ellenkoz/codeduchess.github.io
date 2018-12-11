02188772031883f2e8cd6a6025b4991da2c363bd
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-11 16:03:52 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,61 +1,59 @@
-        //added strict mode to address following error message, "Uncaught SyntaxError: Unexpected token u in JSON at position 0."	
-        'use strict';
-	const app = document.getElementById("root");
+        const app = document.getElementById("root");
 
-	//add API related image
-	const weather = document.createElement("img");
-	weather.src = "weather.jpg";
-	
-	const container = document.createElement("div");
-	container.setAttribute("class", "container");
+    //add API related image
+    const weather = document.createElement("img");
+    weather.src = "weather.jpg";
 
-	//method to append the logo image and container div to the app root.
-	app.appendChild(weather);
-	app.appendChild(container);
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
 
-	// Create a request variable and assign a new XMLHttpRequest object to it.
-	var request = new XMLHttpRequest();
+    //method to append the logo image and container div to the app root.
+    app.appendChild(weather);
+    app.appendChild(container);
 
-	// Open a new connection, using the GET request on the URL endpoint
-	request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=14d276f4fe655e659ec92149c7cebbec", true);
-	
-	request.onload = function () {    
-	  
-	// Begin accessing JSON data here
-	  
-	var data = JSON.parse(this.response);
-	  if (request.status >= 200 && request.status < 400) {
-	    data.weather.forEach(weather => {
-	      const card = document.createElement("div");
-	      card.setAttribute("class", "card");
-	      const h1 = document.createElement("h1");
-	      h1.textContent = weather.title;
-	
-	      const p = document.createElement("p");
-	      weather.description = weather.description.substring(0, 300);
-	      p.textContent = `${weather.description}...`;
-		    
-	      const m = document.createElement("p");
-	      weather.main = weather.main.substring(0, 300);
-	      h1.textContent = `${weather.main}...`;
-		    
-	      const t = document.createElement("p");
-	      weather.temp = weather.temp;
-	      t.textContent = `${data.temp}...`; 
-		    
-	      // Append the cards to the container element
-	      container.appendChild(card);
-		    
-	      // Each card will contain an h1 and a p    
-	      card.appendChild(h1);
-	      card.appendChild(p);
-	      card.appendChils(t);
-	    });
-	  } else {
-	    const errorMessage = document.createElement("marquee");
-	    errorMessage.textContent = `Hug it, it's not working!`;
-	    app.appendChild(errorMessage);
-	  }
-	}
-	// send request
-	request.send();
+    // Create a request variable and assign a new XMLHttpRequest object to 
+    var request = new XMLHttpRequest();
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=14d276f4fe655e659ec92149c7cebbec", true);
+
+    request.onload = function () {    
+    
+    // Begin accessing JSON data here
+
+      var data = JSON.parse(this.response);
+      if (request.status >= 200 && request.status < 400) {
+        data.weather.forEach(weather => {
+          const card = document.createElement("div");
+          card.setAttribute("class", "card");
+          const h1 = document.createElement("h1");
+          h1.textContent = weather.title;
+
+          const p = document.createElement("p");
+          //weather.description = weather.description.substring(0, 300);
+          p.textContent = `${weather.description}...`;
+
+          const m = document.createElement("p");
+          //weather.main = weather.main.substring(0, 300);
+          h1.textContent = `${weather.main}...`;
+
+          const t = document.createElement("p");
+          //weather.main.temp = weather.main.temp;
+          t.textContent = `${data.main.temp}...`;
+
+          // Append the cards to the container element
+          container.appendChild(card);
+
+          // Each card will contain an h1 and a p    
+          card.appendChild(h1);
+          card.appendChild(p);
+          card.appendChild(t);
+        });
+      } else {
+        const errorMessage = document.createElement("marquee");
+        errorMessage.textContent = `Hug it, it's not working!`;
+        app.appendChild(errorMessage);
+      }
+    }
+    // send request
+    request.send();
