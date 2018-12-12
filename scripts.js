f2b1bdfd26e8c4ed68860600ec51c23a703086c4
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-12 13:39:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@
 	      const h1 = document.createElement("h1");
 	      h1.textContent = weather.title;
 	
-	      const p = document.createElement("p");
+	      const describe = document.createElement("p");
 	      weather.description = weather.description.substring(0, 300);
 	      p.textContent = `${weather.description}...`;
 	
