285ad1fbf575232f5734718a053b9094675dbeeb
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-13 17:33:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@
 	app.appendChild(container);
 	
 	var request = new XMLHttpRequest();
-        request.open("GET", "htpps://www.themuse.com/api/public/jobs?category=Creative%20%26%20Design&category=Data%20Science&category=Engineering&category=Project%20%26%20Product%20Management&category=Social%20Media%20%26%20Community&page=1", true);
+        request.open("GET", "https://www.themuse.com/api/public/jobs?category=Creative%20%26%20Design&category=Data%20Science&category=Engineering&category=Project%20%26%20Product%20Management&category=Social%20Media%20%26%20Community&page=1", true);
 	//request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=14d276f4fe655e659ec92149c7cebbec", true);
 	
 	request.onload = function () {    
