846f3eeb2daa8e5a45dc331b1861604bda8df060
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-13 20:37:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@
 	const container = document.createElement("div");
 	container.setAttribute("class", "container");
 	
-	app.appendChild(weather);
+	app.appendChild(results);
 	app.appendChild(container);
 	
 	var request = new XMLHttpRequest();
@@ -20,31 +20,31 @@
 	  
 	var data = JSON.parse(this.response);
 	  if (request.status >= 200 && request.status < 400) {
-	    data.weather.forEach(weather => {
+	    data.results.forEach(weather => {
 	      const card = document.createElement("div");
 	      card.setAttribute("class", "card");
 	      const h1 = document.createElement("h1");
-	      h1.textContent = weather.title;
+	      h1.textContent = results.title;
 	
-	      const describe = document.createElement("p");
-	      weather.description = weather.description.substring(0, 300);
-	      describe.textContent = `${weather.description}...`;
+	      const jobTitle = document.createElement("p");
+	      results.name = results.name.substring(0, 300);
+	     jobtitle.textContent = `${results.name}...`;
 		    
-	      const main = document.createElement("p");
-	      weather.main = weather.main.substring(0, 300);
-	      main.textContent = `${weather.main}...`;
+	     // const main = document.createElement("p");
+	      //weather.main = weather.main.substring(0, 300);
+	      //main.textContent = `${weather.main}...`;
 		    
-	      const temp = document.createElement("p");
-	      temp.textContent = `${data.main.temp}...`;
+	     // const temp = document.createElement("p");
+	     // temp.textContent = `${data.main.temp}...`;
 		    
 	    //  const temp = document.createElement("p");
           //weather.main.temp = weather.main.temp;
           //temp.textContent = `${data.main.temp}...`;	    
 	
 	      container.appendChild(card);
-	      card.appendChild(main);
-	      card.appendChild(describe);
-	      card.appendChild(temp);
+	      card.appendChild(name);
+	      //card.appendChild(describe);
+	      //card.appendChild(temp);
 	    });
 	  } else {
 	    const errorMessage = document.createElement("marquee");
