927a3d80c6717da168a01faab2ce1afdf31ced87
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-18 07:54:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,12 +19,12 @@
 	      const h1 = document.createElement("h1");
 	      h1.textContent = title;
 	
-	      const jobDesc = document.createElement("p");
-	      description = description.substring(0, 300);
-	      jobDesc.textContent = `${data.description}...`;
+	      const title = document.createElement("p");
+	      title = title.substring(0, 300);
+	      title.textContent = `${data.title}...`;
 		    
 	      container.appendChild(card);
-	      card.appendChild(jobDesc);
+	      card.appendChild(title);
 	   
 	    });
 	  } else {
