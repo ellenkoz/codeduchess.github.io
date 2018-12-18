cde619674aa41e5efebe7b6c11f5a87320063e69
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-18 08:06:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,6 +12,11 @@
        
 	request.onload = function () {    
 	    // Begin accessing JSON data here
+	var data = JSON.parse(this.response);
+		
+	data.forEach(job => {
+	// log each job title
+	console.log(job.title);
 	}
 	// send request
 	request.send();
