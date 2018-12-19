236ca77f65b0090aed321947a09cc50300d98031
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-18 21:00:37 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 'use strict';
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+//const logo = document.createElement('img');
+//logo.src = 'logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
+//app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
