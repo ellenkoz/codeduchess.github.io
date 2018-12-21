d43e36673adbf370a1e33e1c2f43054bef729585
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-21 08:37:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,11 +22,11 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = job.name;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      job.contents = job.contents.substring(0, 300);
+      p.textContent = `${job.contents}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
