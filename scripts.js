79aa2d79bce3cc82d32c14814cb8fbd40d02d099
Dana L. Brown <dana.l.brown@gmail.com>
2018-12-21 08:38:55 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.results.forEach(movie => {
+    data.results.forEach(job => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
