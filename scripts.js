b692075e9e74da6e648c3a6bebde1a66309d4aa5
Dana L. Brown <dana.l.brown@gmail.com>
2019-01-24 15:41:10 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,38 +40,4 @@ request.onload = function () {
   }
 }
 
-request.send(); */
-
- 
-var request = new XMLHttpRequest();
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
-request.onload = function () {
-  // Begin accessing JSON data here
-  }
-}
-
-// Send request
-request.send();
-
-// Begin accessing JSON data here
-var data = JSON.parse(this.response);
-
-data.forEach(movie => {
-  // Log each movie's title
-  console.log(movie.title);
-});
-
-
-
-
-
-
-
-
-
-
-
-
+request.send(); 
