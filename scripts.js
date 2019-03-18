04b312ff7c8a8fb8ee5e566a12ef8ab4063ac0ed
ellenkoz <32963035+ellenkoz@users.noreply.github.com>
2019-03-18 14:21:52 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -54,18 +54,19 @@
         
       }
         
-     const jobs="apiData";       
+     //const jobs=apiData; 
+     const jobs = Object.assign({}, apiData)
     // whenever a user types a character in the search bar it is a keyup event -- we will listen for those    
     userInput.addEventListener('keyup', function(e){
     // the target is the input field, so we are asking for its value  
 	const term = e.target.value.toLowerCase();
       
     // here we access the apiData and target the h1 tag because the job titles are stored there
-	//const jobs = apiData.getElementsByTagName('h1');
+      const jobs = apiData.getElementsByTagName('h1'); 
      
     // now we cycle through the h1 tags to see if the terms from the search are contained in the h1 tag
-    // added Array.prototype to solve "jobs.forEach is not a function" error  
-	Array.prototype.jobs.map(function(job){
+    // added Array.prototype to solve "jobs.forEach is not a function" error
+	jobs.forEach(jobs.map(function(job){
     // not sure if this is correct - should I put the index number in the array where title is located in the JSON?
 		const title= job.textContent;
 		if(title.toLowerCase().indexOf(term) != -1){
@@ -76,7 +77,9 @@
                   
 	     	}
 		})
-	});   
+	    
+	}));   
+
             request.send();
  
                      
